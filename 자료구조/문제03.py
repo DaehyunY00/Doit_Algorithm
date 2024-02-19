5e362b15f1ab0a1b8c8085da728c67018efd8bb4
holangdoongee <dh.yoo117@gmail.com>
2024-02-19 17:47:58 +0900

    Update 문제03.py

--- 자료구조/문제03.py
+++ 자료구조/문제03.py
@@ -1,14 +1,23 @@
-import sys
+# import sys
 
+# input = sys.stdin.readline
+# n, m = map(int, input().split())
+# numbers = list(map(int, input().split()))
+
+# dp = [0] * (n + 1) # 길이가 n+1 인 dp 배열을 0으로 초기화. 각 부분의 부분합을 저장.
+
+# for i in range(n):
+#     dp[i + 1] = dp[i] + numbers[i] # dp[i]는 numbers 배열의 첫 번째 원소부터 i-1번째 원소까지의 합을 저장
+    
+# for i in range(m):
+#     i, j = map(int, input().split())
+#     print(dp[j] - dp[i - 1])
+
+import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
 numbers = list(map(int, input().split()))
 
-dp = [0] * (n + 1) # 길이가 n+1 인 dp 배열을 0으로 초기화. 각 부분의 부분합을 저장.
-
-for i in range(n):
-    dp[i + 1] = dp[i] + numbers[i] # dp[i]는 numbers 배열의 첫 번째 원소부터 i-1번째 원소까지의 합을 저장
-    
-for i in range(m):
+for _ in range(m):
     i, j = map(int, input().split())
-    print(dp[j] - dp[i - 1])
+    print(sum(numbers[i-1:j]))        
